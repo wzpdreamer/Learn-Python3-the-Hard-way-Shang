cba4b0382d9de776b14b7a0b855c215930f6a3ad
wzpdreamer <3176869767@qq.com>
2019-03-08 21:12:30 +0800

    njsglj

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,5 @@ months="Jan Feb Mar Apr May Jun Jul Aug"
 print("Here are the days:",days)
 print("Here are the months:",months)
 print("""There's something going on here.""")
+age=input("How old are you?")
+print(f"you're {age} old.")
