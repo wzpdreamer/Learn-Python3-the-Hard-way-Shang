34ea82fef130fc67cb34113e0f6a8b02d5d793b0
wzpdreamer <3176869767@qq.com>
2019-03-08 20:03:42 +0800

    一个py文件

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan Feb Mar Apr May Jun Jul Aug"
+print("Here are the days:",days)
+print("Here are the months:",months)
+print("""There's something going on here.""")
