6b93cf7feee603baa6f6900b5b155ef12d100da2
wzpdreamer <3176869767@qq.com>
2019-03-08 20:36:08 +0800

    text

---
+++ njsglj.py
@@ -0,0 +1,13 @@
+tabby_cat="\t I'm tabbed in."
+persian_cat="I'm split\nnon a line."
+backslash_cat="I'm \\ a \\ cat."
+fat_cat="""
+I'll do a list:
+\t*Cat Food
+\t*Fishes
+\t*Catnip\n\t*Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
